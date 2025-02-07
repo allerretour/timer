196efcf9f6fefa0515ad1ff30d8100b137ec00da
allerretour <aeuui.dvdps@gmail.com>
2025-02-07 17:17:36 -0500

    Add files via upload

--- shortcuts.js
+++ shortcuts.js
@@ -50,7 +50,7 @@ const gamepadMapping = {
 
 // Add joystick mappings
 const joystickSensitivity = 0.5;  // 0.0 → No movement at all.  1.0 → Full movement based on input.
-const joystickDeadzone = 0.3;     // 0.0 → No deadzone (detects even the smallest movements).  0.2 - 0.3 → Common values to filter out joystick drift.
+const joystickDeadzone = 0.8;     // 0.0 → No deadzone (detects even the smallest movements).  0.2 - 0.3 → Common values to filter out joystick drift.
 
 window.addEventListener("gamepadconnected", (event) => {
     gamepadIndex = event.gamepad.index;
