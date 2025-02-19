5156d680da95b8bebe7599cc7b443431e44bd031
allerretour <aeuui.dvdps@gmail.com>
2025-02-19 10:39:00 -0500

    Add files via upload

--- shortcuts.js
+++ shortcuts.js
@@ -6,26 +6,29 @@ const keyActions = {
     "r": openSettings,
     "d": toggleBoutonsRonds,
     "i": openInstructions,
-    "Alt": resetToNextValue,
-    "Control": resetTimer,
-    "1": reloadPage,
-    "2": hideSplashScreen,
-    "9": resetScores,
-    " ": pauseTimer,
-    "v": toggleFullscreen,
-    "Shift": toggleVisibility,    
+    "c": resetToNextValue,
+    "b": resetTimer,
+    "w": reloadPage,
+    "e": hideSplashScreen,
+    "p": resetScores,
+    "v": pauseTimer,
+    "u": toggleFullscreen,
+    "f": toggleVisibility,    
     "a": () => document.getElementById("p1plus").click(),
     "q": () => document.getElementById("p1moins").click(),
     "k": () => document.getElementById("p2plus").click(),
     "o": () => document.getElementById("p2moins").click(),
 
-    "3": toggleVisibility,
-    "4": resetToNextValue,
-    "5": pauseTimer,
-    "6": resetTimer,
-    "7": () => document.getElementById("p1plus").click(),
-    "8": () => document.getElementById("p2plus").click(),
-    "0": zoomOut
+    "5": toggleVisibility,
+    "7": resetToNextValue,
+    "8": pauseTimer,
+    "9": resetTimer,
+    "1": () => document.getElementById("p1plus").click(),
+    "3": () => document.getElementById("p2plus").click(),
+     "2": resetScores,
+     "4": () => addTime('addButton'),
+      "6": () => addTime('addButton2'),
+     "0": reloadPage
 };
 
 // Keydown Event Listener
@@ -45,9 +48,9 @@ let gamepadPolling = false;
 
 // Button mappings (individual buttons)
 const gamepadMapping = {
-    0: " ",        // Button 0 (usually 'A' on Xbox/standard gamepad)
-    1: "Control",  // Button 1 (usually 'B' on Xbox/standard gamepad)
-    2: "Alt",      // Button 2 (usually 'X' on Xbox/standard gamepad)
+    0: "c",        // Button 0 (usually 'A' on Xbox/standard gamepad)
+    1: "v",  // Button 1 (usually 'B' on Xbox/standard gamepad)
+    2: "b",      // Button 2 (usually 'X' on Xbox/standard gamepad)
     3: "m",        // Button 3 (usually 'Y' on Xbox/standard gamepad)
     4: "a",        // Button 4 (Left Bumper - 'LB')
     5: "k",        // Button 5 (Right Bumper - 'RB')
@@ -55,10 +58,10 @@ const gamepadMapping = {
     7: "o",        // Button 7 (Right Trigger - 'RT')
     8: "i",        // Button 8 (Back button - 'Select')
     9: "r",        // Button 9 (Start button - 'Start')
-    10: "9",       // Button 10 (Left Stick Button - L3)
-    11: "1",       // Button 11 (Right Stick Button - R3)
-    12: "z",       // Button 12 (D-Pad Up)
-    13: "Shift",   // Button 13 (D-Pad Down)
+    10: "p",       // Button 10 (Left Stick Button - L3)
+    11: "w",       // Button 11 (Right Stick Button - R3)
+    12: "d",       // Button 12 (D-Pad Up)
+    13: "f",   // Button 13 (D-Pad Down)
     14: "t",       // Button 14 (D-Pad Left)
     15: "y",       // Button 15 (D-Pad Right)
     16: "d"        // Button 16 (custom or additional button)
@@ -66,7 +69,7 @@ const gamepadMapping = {
 
 // Define multiple button combinations and their corresponding actions
 const comboMappings = {
-    "6+7": "2",     // Buttons 4 and 5 together trigger key "2"
+    "6+7": "e",     // Buttons 4 and 5 together trigger key "2"
     "0+3": "m",     // Buttons 0 and 3 together trigger key "m"
     "1+2+3": "Alt"  // Buttons 1, 2, and 3 together trigger key "Alt"
 };
